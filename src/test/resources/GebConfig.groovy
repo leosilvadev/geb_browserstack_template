0fcc47e9155b5b0c7f21ba9e9177b245d253b0f4
Leonardo Silva <leosilvadev@gmail.com>
2018-09-19 23:46:33 +0200

    define some default tasks

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,22 +1,22 @@
 import geb.driver.BrowserStackDriverFactory
 
 waiting {
-    timeout = 10
+  timeout = 10
 }
 
 def browserStackBrowser = System.getProperty('geb.browserstack.browser')
 if (browserStackBrowser) {
-    driver = {
-        def username = System.getenv('GEB_BROWSERSTACK_USERNAME')
-        assert username
-        def accessKey = System.getenv('GEB_BROWSERSTACK_AUTHKEY')
-        assert accessKey
+  driver = {
+    def username = System.getenv('GEB_BROWSERSTACK_USERNAME')
+    assert username
+    def accessKey = System.getenv('GEB_BROWSERSTACK_AUTHKEY')
+    assert accessKey
 
-        def capabilities = [
-                build: System.getenv('GEB_BROWSERSTACK_BUILD') ?: 'Default build',
-                name: new Date().format("dd/MM/yyyy HH:mm:ss"),
-                project: System.getenv('GEB_BROWSERSTACK_PROJECT') ?: 'Project Geb test'
-        ]
-        new BrowserStackDriverFactory().create(browserStackBrowser, username, accessKey, capabilities)
-    }
+    def capabilities = [
+      build  : System.getenv('GEB_BROWSERSTACK_BUILD') ?: 'Default build',
+      name   : new Date().format("dd/MM/yyyy HH:mm:ss"),
+      project: System.getenv('GEB_BROWSERSTACK_PROJECT') ?: 'Project Geb test'
+    ]
+    new BrowserStackDriverFactory().create(browserStackBrowser, username, accessKey, capabilities)
+  }
 }
