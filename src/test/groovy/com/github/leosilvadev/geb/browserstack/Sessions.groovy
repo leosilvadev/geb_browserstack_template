dc3c8f4a5f7a1be369f225dabffff8663f8c00a2
Leonardo Silva <leosilvadev@gmail.com>
2018-09-20 21:24:02 +0200

    update session name after test run

--- src/test/groovy/com/github/leosilvadev/geb/browserstack/Sessions.groovy
+++ src/test/groovy/com/github/leosilvadev/geb/browserstack/Sessions.groovy
@@ -9,11 +9,12 @@ import static java.lang.String.format
 class Sessions {
 
   private static final String URL_GET_SESSIONS = config.urlFor('/builds/%s/sessions.json')
-  private static final String URL_UPDATE_SESSION = config.urlFor('/sessions/%s.json')
+  private static final String URL_ONE_SESSION = config.urlFor('/sessions/%s.json')
   private static final ObjectMapper mapper = new ObjectMapper()
 
   static Map currentSession() {
     def build = Builds.currentBuild()
+
     def json = Unirest.get(format(URL_GET_SESSIONS, build.automation_build.hashed_id))
       .basicAuth(config.username(), config.accessKey())
       .asString()
@@ -23,10 +24,19 @@ class Sessions {
     sessions ? sessions.head() : [:]
   }
 
+  static void setSessionName(final String name) {
+    def id = currentSession().automation_session.hashed_id
+    Unirest.put(format(URL_ONE_SESSION, id))
+      .header('Content-Type', 'application/json')
+      .basicAuth(config.username(), config.accessKey())
+      .body(mapper.writeValueAsString([name: name]))
+      .asString()
+      .body
+  }
 
   static void failCurrentSession(Throwable ex) {
     def id = currentSession().automation_session.hashed_id
-    Unirest.put(format(URL_UPDATE_SESSION, id))
+    Unirest.put(format(URL_ONE_SESSION, id))
       .header('Content-Type', 'application/json')
       .basicAuth(config.username(), config.accessKey())
       .body(mapper.writeValueAsString([status: 'failed', reason: ex.message]))
--- src/test/groovy/com/github/leosilvadev/geb/specs/FeatureSpec.groovy
+++ src/test/groovy/com/github/leosilvadev/geb/specs/FeatureSpec.groovy
@@ -1,11 +1,14 @@
 package com.github.leosilvadev.geb.specs
 
+import com.github.leosilvadev.geb.browserstack.Sessions
 import geb.spock.GebReportingSpec
 
 class FeatureSpec extends GebReportingSpec {
 
   def cleanup() {
-    System.setProperty('geb.browserstack.test.name', "Test name")
+    def specName = specificationContext.currentSpec.name
+    def featureName = specificationContext.currentFeature.name
+    Sessions.setSessionName("${specName}: ${featureName}")
   }
 
 }
