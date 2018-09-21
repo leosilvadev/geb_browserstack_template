e88cd25082ddc51ec6b0de143f96cab38f6d4243
Leonardo Silva <leosilvadev@gmail.com>
2018-09-21 23:19:49 +0200

    set a new session mode and add the comment

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,3 +1,4 @@
+import com.github.leosilvadev.geb.browserstack.config.BrowserstackConfig
 import geb.driver.BrowserStackDriverFactory
 import com.github.leosilvadev.geb.browserstack.config.BrowserstackConfig as config
 
@@ -7,6 +8,20 @@ waiting {
 
 def browserStackBrowser = System.getProperty('geb.browserstack.browser')
 if (browserStackBrowser) {
+
+  /**
+   * Define the way you want to manager your browserstack sessions:
+   * - SESSION_PER_SPEC:
+   * create a new session in browserstack for each new specification and
+   * record all the tests methods in this session.
+   * This mode cleans the cache and close the browser after the method execution
+   *
+   *
+   * - SESSION_PER_METHOD:
+   * create a new session for each method in each specification.
+   * This mode keeps the cache and the opened browser for the whole specification
+   **/
+  BrowserstackConfig.setMode(BrowserstackConfig.SessionMode.SESSION_PER_METHOD)
   driver = {
     def capabilities = [
       build  : config.buildName(),
