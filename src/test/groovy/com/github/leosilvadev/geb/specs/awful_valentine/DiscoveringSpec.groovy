6e11777b603391b0e7c52b18108fb692f113b758
Leonardo Silva <leosilvadev@gmail.com>
2018-09-30 00:13:29 +0200

    adding spec description

--- src/test/groovy/com/github/leosilvadev/geb/specs/awful_valentine/DiscoveringSpec.groovy
+++ src/test/groovy/com/github/leosilvadev/geb/specs/awful_valentine/DiscoveringSpec.groovy
@@ -4,17 +4,16 @@ import com.github.leosilvadev.geb.pages.awful_valentine.CartPage
 import com.github.leosilvadev.geb.pages.awful_valentine.HomePage
 import com.github.leosilvadev.geb.specs.FeatureSpec
 
-class DiscoveryingSpec extends FeatureSpec {
+class DiscoveringSpec extends FeatureSpec {
 
     def 'Customer should go to home page, then go to cart page and see an empty cart'() {
-        when:
+        when: 'the customer goes to the home page'
         to(HomePage)
 
-        and:
+        and: 'he clicks on "Cart" link'
         at(HomePage).header.go_to_cart()
 
-
-        then:
+        then: 'he should be redirected to the cart page and see no item on it'
         at(CartPage).has_no_items()
     }
 
