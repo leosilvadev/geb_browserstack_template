21397a1a594c6b0e4317e4aa293de4943f78cefc
Leonardo Silva <leosilvadev@gmail.com>
2018-10-11 00:06:41 +0200

    first spec version

--- src/test/groovy/com/github/leosilvadev/geb/specs/awful_valentine/contact/ContactSendValidationSpec.groovy
+++ src/test/groovy/com/github/leosilvadev/geb/specs/awful_valentine/contact/ContactSendValidationSpec.groovy
@@ -2,10 +2,11 @@ package com.github.leosilvadev.geb.specs.awful_valentine.contact
 
 import com.github.leosilvadev.geb.pages.awful_valentine.ContactPage
 import com.github.leosilvadev.geb.specs.FeatureSpec
+import geb.navigator.Navigator
 
 class ContactSendValidationSpec extends FeatureSpec {
 
-    def 'Customer must not be able to send contact without set his name'(String name, String email, String subject, String message) {
+    def 'Customer must not be able to send contact without set his name'(String name, String email, String subject, String message, String class_name) {
         when: 'the customer goes to the contact page'
         to(ContactPage)
 
@@ -17,11 +18,13 @@ class ContactSendValidationSpec extends FeatureSpec {
         at(ContactPage).submit()
 
         then:
-        at(ContactPage)
+        at(ContactPage).hasAlertFor(class_name)
 
         where:
-        name    | email               | subject   | message
-        ''      | 'dummy@email.com'   | 'hello'   | 'Hello! Can you help me?'
+        name    | email               | subject   | message                     | class_name
+        ''      | 'dummy@email.com'   | 'hello'   | 'Hello! Can you help me?'   | 'your-name'
+        'name'  | ''                  | 'hello'   | 'Hello! Can you help me?'   | 'your-email'
+        'name'  | 'dummynoemail'      | 'hello'   | 'Hello! Can you help me?'   | 'your-email'
     }
 
 }
